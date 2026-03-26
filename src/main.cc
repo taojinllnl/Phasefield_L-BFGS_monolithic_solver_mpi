02d5aea21aa1244977b3c597665f07cbb5d492ee
Zhao Li <zli385@uottawa.ca>
2026-03-26 08:21:01 -0400

    prepare data type change in local_refine_and_solution_transfer

--- src/main.cc
+++ src/main.cc
@@ -6038,19 +6038,32 @@ template <typename LATraits, typename Tria>
   }
 
 
-  template <typename LATraits, typename Tria>
-  bool PhaseFieldMonolithicSolve<LATraits, Tria>::local_refine_and_solution_transfer(BlockVector<double> & solution_delta,
-									  BlockVector<double> & LBFGS_update_refine)
-  {
+template <typename LATraits, typename Tria>
+bool PhaseFieldMonolithicSolve<LATraits, Tria>::local_refine_and_solution_transfer(BVector & solution_delta,
+                                                                                   BVector & LBFGS_update_refine)
+{
     // This is the solution at (n+1) obtained from the old (coarse) mesh
-    BlockVector<double> solution_next_step(m_dofs_per_block);
-    solution_next_step = m_solution + solution_delta;
+    BVector solution_next_step(m_mpiInfo, m_blocks_desc, /*relevance=*/true);
+    solution_next_step.initialize();
+    solution_next_step.base() = m_solution.base() + solution_delta.base();
+    solution_next_step.updateRelevance();
+    
+    
     bool mesh_is_same = true;
     bool cell_refine_flag = true;
-
+    
     unsigned int material_id;
     double length_scale;
     double cell_length;
+    
+    
+    // target H-vectors to avoid recalculating
+    using VecType  = typename BVector::VecType;
+    using VecBType = typename LATraits::VectorBlock;
+    
+    VecBType new_history_variable_field_L2;
+    VecBType new_history_variable_field_L2_rele;
+    
     while(cell_refine_flag)
     {
         cell_refine_flag = false;
@@ -6058,44 +6071,53 @@ template <typename LATraits, typename Tria>
         std::vector<types::global_dof_index> local_dof_indices(m_fe.dofs_per_cell);
         for (const auto &cell : m_dof_handler.active_cell_iterators())
         {
-              cell->get_dof_indices(local_dof_indices);
-              
-              for (unsigned int i = 0; i< m_fe.dofs_per_cell; ++i)
-              {
-                  const unsigned int comp_i = m_fe.system_to_component_index(i).first;
-                  if (comp_i == m_d_component) //phasefield component
-                  {
-                      if (  solution_next_step(local_dof_indices[i])
-                          > m_parameters.m_phasefield_refine_threshold )
-                      {
-                          material_id = cell->material_id();
-                          length_scale = m_material_data[material_id][2];
-                          if (dim == 2)
-                              cell_length = std::sqrt(cell->measure());
-                          else
-                              cell_length = std::cbrt(cell->measure());
-                          if (  cell_length
-                              > length_scale * m_parameters.m_allowed_max_h_l_ratio )
-                          {
-                              if (cell->level() < m_parameters.m_max_allowed_refinement_level)
-                              {
-                                  cell->set_refine_flag();
-                                  break;
-                              }
-                          }
-                      }
-                  }
-              }
-          }
-          
-          for (const auto &cell : m_dof_handler.active_cell_iterators())
-          {
-              if (cell->refine_flag_set())
-              {
-                  cell_refine_flag = true;
-                  break;
-              }
-          }
+            
+            if constexpr (is_mpi) {
+                if (!cell->is_locally_owned()) continue;
+            }
+            
+            cell->get_dof_indices(local_dof_indices);
+            
+            for (unsigned int i = 0; i< m_fe.dofs_per_cell; ++i)
+            {
+                const unsigned int comp_i = m_fe.system_to_component_index(i).first;
+                if (comp_i == m_d_component) //phasefield component
+                {
+                    if (  solution_next_step(local_dof_indices[i])
+                        > m_parameters.m_phasefield_refine_threshold )
+                    {
+                        material_id = cell->material_id();
+                        length_scale = m_material_data[material_id][2];
+                        if (dim == 2)
+                            cell_length = std::sqrt(cell->measure());
+                        else
+                            cell_length = std::cbrt(cell->measure());
+                        if (  cell_length
+                            > length_scale * m_parameters.m_allowed_max_h_l_ratio )
+                        {
+                            if (cell->level() < m_parameters.m_max_allowed_refinement_level)
+                            {
+                                cell->set_refine_flag();
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        for (const auto &cell : m_dof_handler.active_cell_iterators())
+        {
+            if constexpr (is_mpi) {
+                if (!cell->is_locally_owned()) continue;;
+            }
+            
+            if (cell->refine_flag_set())
+            {
+                cell_refine_flag = true;
+                break;
+            }
+        }
         
         
         if constexpr (is_mpi)
@@ -6107,144 +6129,146 @@ template <typename LATraits, typename Tria>
             cell_refine_flag = (global_flag > 0u);
         }
           
-          // if any cell is refined, we need to project the solution
-          // to the newly refined mesh
-          if (cell_refine_flag)
-          {
-              mesh_is_same = false;
-              
-              std::vector<BlockVector<double>> old_solutions(2);
-              old_solutions[0] = solution_next_step.base();
-              old_solutions[1] = m_solution.base();
-              
-              // history variable field L2 projection
-              DoFHandler<dim> dof_handler_L2(m_triangulation);
-              FE_DGQ<dim>     fe_L2(m_parameters.m_poly_degree); //Discontinuous Galerkin
-              dof_handler_L2.distribute_dofs(fe_L2);
-              AffineConstraints<double> constraints;
-              constraints.clear();
-              //Since we use discontinuous Lagrange polynomials as shape functions
-              //we don't need to worry about enforcing continuity of the history variable
-              //at hanging nodes.
-              //DoFTools::make_hanging_node_constraints(dof_handler_L2, constraints);
-              constraints.close();
-              
-              Vector<double> old_history_variable_field_L2;
-              old_history_variable_field_L2.reinit(dof_handler_L2.n_dofs());
-              
-              MappingQ<dim> mapping(m_parameters.m_poly_degree + 1);
-              VectorTools::project(mapping,
-                                   dof_handler_L2,
-                                   constraints,
-                                   m_qf_cell,
-                                   [&] (const typename DoFHandler<dim>::active_cell_iterator & cell,
-                                        const unsigned int q) -> double
-                                   {
-                  return m_quadrature_point_history.get_data(cell)[q]->get_history_max_positive_strain_energy();
-              },
-                                   old_history_variable_field_L2);
-              
-              m_triangulation.prepare_coarsening_and_refinement();
-              SolutionTransfer<dim, BlockVector<double>> solution_transfer(m_dof_handler);
-              solution_transfer.prepare_for_coarsening_and_refinement(old_solutions);
-              SolutionTransfer<dim, Vector<double>> solution_transfer_history_variable(dof_handler_L2);
-              solution_transfer_history_variable.prepare_for_coarsening_and_refinement(old_history_variable_field_L2);
-              m_triangulation.execute_coarsening_and_refinement();
-              
-              setup_system();
-              
-              dof_handler_L2.distribute_dofs(fe_L2);
-              constraints.clear();
-              //Since we use discontinuous Lagrange polynomials as shape functions
-              //we don't need to worry about enforcing continuity of the history variable
-              //at hanging nodes.
-              //DoFTools::make_hanging_node_constraints(dof_handler_L2, constraints);
-              constraints.close();
-              
-              std::vector<BVector> tmp_solutions;
-              tmp_solutions.emplace_back(m_mpiInfo, m_blocks_desc, /*relevance=*/false);
-              tmp_solutions.emplace_back(m_mpiInfo, m_blocks_desc, /*relevance=*/false);
-              tmp_solutions[0].initialize();
-              tmp_solutions[1].initialize();
-              //	    tmp_solutions[0].reinit(m_dofs_per_block);
-              //	    tmp_solutions[1].reinit(m_dofs_per_block);
-              
-              Vector<double> new_history_variable_field_L2;
-              new_history_variable_field_L2.reinit(dof_handler_L2.n_dofs());
-              
+        // if any cell is refined, we need to project the solution
+        // to the newly refined mesh
+        if (cell_refine_flag)
+        {
+            mesh_is_same = false;
+            
+            std::vector<BlockVector<double>> old_solutions(2);
+            old_solutions[0] = solution_next_step.base();
+            old_solutions[1] = m_solution.base();
+            
+            // history variable field L2 projection
+            DoFHandler<dim> dof_handler_L2(m_triangulation);
+            FE_DGQ<dim>     fe_L2(m_parameters.m_poly_degree); //Discontinuous Galerkin
+            dof_handler_L2.distribute_dofs(fe_L2);
+            AffineConstraints<double> constraints;
+            constraints.clear();
+            //Since we use discontinuous Lagrange polynomials as shape functions
+            //we don't need to worry about enforcing continuity of the history variable
+            //at hanging nodes.
+            //DoFTools::make_hanging_node_constraints(dof_handler_L2, constraints);
+            constraints.close();
+            
+            Vector<double> old_history_variable_field_L2;
+            old_history_variable_field_L2.reinit(dof_handler_L2.n_dofs());
+            
+            MappingQ<dim> mapping(m_parameters.m_poly_degree + 1);
+            VectorTools::project(mapping,
+                                 dof_handler_L2,
+                                 constraints,
+                                 m_qf_cell,
+                                 [&] (const typename DoFHandler<dim>::active_cell_iterator & cell,
+                                      const unsigned int q) -> double
+                                 {
+                return m_quadrature_point_history.get_data(cell)[q]->get_history_max_positive_strain_energy();
+            },
+                                 old_history_variable_field_L2);
+            
+            m_triangulation.prepare_coarsening_and_refinement();
+            SolutionTransfer<dim, BlockVector<double>> solution_transfer(m_dof_handler);
+            solution_transfer.prepare_for_coarsening_and_refinement(old_solutions);
+            SolutionTransfer<dim, Vector<double>> solution_transfer_history_variable(dof_handler_L2);
+            solution_transfer_history_variable.prepare_for_coarsening_and_refinement(old_history_variable_field_L2);
+            m_triangulation.execute_coarsening_and_refinement();
+            
+            setup_system();
+            
+            dof_handler_L2.distribute_dofs(fe_L2);
+            constraints.clear();
+            //Since we use discontinuous Lagrange polynomials as shape functions
+            //we don't need to worry about enforcing continuity of the history variable
+            //at hanging nodes.
+            //DoFTools::make_hanging_node_constraints(dof_handler_L2, constraints);
+            constraints.close();
+            
+            std::vector<BVector> tmp_solutions;
+            tmp_solutions.emplace_back(m_mpiInfo, m_blocks_desc, /*relevance=*/false);
+            tmp_solutions.emplace_back(m_mpiInfo, m_blocks_desc, /*relevance=*/false);
+            tmp_solutions[0].initialize();
+            tmp_solutions[1].initialize();
+            //	    tmp_solutions[0].reinit(m_dofs_per_block);
+            //	    tmp_solutions[1].reinit(m_dofs_per_block);
+            
+            Vector<double> new_history_variable_field_L2;
+            new_history_variable_field_L2.reinit(dof_handler_L2.n_dofs());
+            
 #  if DEAL_II_VERSION_GTE(9, 7, 0)
-              solution_transfer.interpolate(tmp_solutions);
+            solution_transfer.interpolate(tmp_solutions);
 #  else
-              // If an older version of dealII is used, for example, 9.4.0, interpolate()
-              // needs to use the following interface.
-              solution_transfer.interpolate(old_solutions, tmp_solutions);
-            #  endif
-
-            #  if DEAL_II_VERSION_GTE(9, 7, 0)
-              solution_transfer_history_variable.interpolate(new_history_variable_field_L2);
-            #  else
-	    // If an older version of dealII is used, for example, 9.4.0, interpolate()
+            // If an older version of dealII is used, for example, 9.4.0, interpolate()
             // needs to use the following interface.
-              solution_transfer_history_variable.interpolate(old_history_variable_field_L2, new_history_variable_field_L2);
-            #  endif
-
-	    solution_next_step = tmp_solutions[0];
-	    m_solution = tmp_solutions[1];
-
-	    // make sure the projected solutions still satisfy
-	    // hanging node constraints
-	    m_constraints.distribute(solution_next_step);
-	    m_constraints.distribute(m_solution);
-	    //Since we use discontinuous Lagrange polynomials as shape functions
-	    //we don't need to worry about enforcing continuity of the history variable
-	    //at hanging nodes.
-	    //constraints.distribute(new_history_variable_field_L2);
-
+            solution_transfer.interpolate(old_solutions, tmp_solutions);
+#  endif
+            
+#  if DEAL_II_VERSION_GTE(9, 7, 0)
+            solution_transfer_history_variable.interpolate(new_history_variable_field_L2);
+#  else
+            // If an older version of dealII is used, for example, 9.4.0, interpolate()
+            // needs to use the following interface.
+            solution_transfer_history_variable.interpolate(old_history_variable_field_L2, new_history_variable_field_L2);
+#  endif
+            
+            solution_next_step = tmp_solutions[0];
+            m_solution = tmp_solutions[1];
+            
+            // make sure the projected solutions still satisfy
+            // hanging node constraints
+            m_constraints.distribute(solution_next_step);
+            m_constraints.distribute(m_solution);
+            //Since we use discontinuous Lagrange polynomials as shape functions
+            //we don't need to worry about enforcing continuity of the history variable
+            //at hanging nodes.
+            //constraints.distribute(new_history_variable_field_L2);
+            
             // new_history_variable_field_L2 contains the history variable projected
             // onto the newly refined mesh
-	    FEValues<dim> fe_values(fe_L2,
-				    m_qf_cell,
-				    update_values | update_gradients |
-				    update_quadrature_points | update_JxW_values);
-
-	    for (const auto &cell : dof_handler_L2.active_cell_iterators())
-	      {
-	        fe_values.reinit(cell);
-
-	        const std::vector<std::shared_ptr<PointHistory<dim>>> lqph =
-	              m_quadrature_point_history.get_data(cell);
-
-	        std::vector<double> history_variable_values_cell(m_n_q_points);
-
-	        fe_values.get_function_values(
-	            new_history_variable_field_L2, history_variable_values_cell);
-
-	        for (unsigned int q_point : fe_values.quadrature_point_indices())
-	          {
-	            lqph[q_point]->assign_history_variable(history_variable_values_cell[q_point]);
-	          }
-	      }
-	  } // if (cell_refine_flag)
-      } // while(cell_refine_flag)
-
+            FEValues<dim> fe_values(fe_L2,
+                                    m_qf_cell,
+                                    update_values | update_gradients |
+                                    update_quadrature_points | update_JxW_values);
+            
+            for (const auto &cell : dof_handler_L2.active_cell_iterators())
+            {
+                fe_values.reinit(cell);
+                
+                const std::vector<std::shared_ptr<PointHistory<dim>>> lqph =
+                m_quadrature_point_history.get_data(cell);
+                
+                std::vector<double> history_variable_values_cell(m_n_q_points);
+                
+                fe_values.get_function_values(
+                                              new_history_variable_field_L2, history_variable_values_cell);
+                
+                for (unsigned int q_point : fe_values.quadrature_point_indices())
+                {
+                    lqph[q_point]->assign_history_variable(history_variable_values_cell[q_point]);
+                }
+            }
+        } // if (cell_refine_flag)
+    } // while(cell_refine_flag)
+    
     // calculate field variables for newly refined cells
     if (!mesh_is_same)
-      {
-	BlockVector<double> temp_solution_delta(m_dofs_per_block);
-	temp_solution_delta = 0.0;
-	update_qph_incremental(temp_solution_delta, m_solution, false);
-	//Since we want to map the history variable in the previous time step
-	//from the coarse mesh to the refined mesh, we should not update them here.
-	//update_history_field_step();
-
-	m_logfile << "\t\tUpdate field variables" << std::endl;
-
-	// initial guess for the resolve on the refined mesh
-	LBFGS_update_refine = solution_next_step - m_solution;
-      }
-
+    {
+        BVector temp_solution_delta(m_mpiInfo, m_blocks_desc, /*relevance=*/true);
+        temp_solution_delta.initialize();
+        
+        
+        update_qph_incremental(temp_solution_delta, m_solution, false);
+        //Since we want to map the history variable in the previous time step
+        //from the coarse mesh to the refined mesh, we should not update them here.
+        //update_history_field_step();
+        
+        m_logfile << "\t\tUpdate field variables" << std::endl;
+        
+        // initial guess for the resolve on the refined mesh
+        LBFGS_update_refine.base() = solution_next_step.base() - m_solution.base();
+    }
+    
     return mesh_is_same;
-  }
+}
 
   template <typename LATraits, typename Tria>
   void PhaseFieldMonolithicSolve<LATraits, Tria>::print_parameter_information()
