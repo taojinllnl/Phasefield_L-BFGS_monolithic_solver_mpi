0aa6b5d606768d573a6ae07f1381364e42fe82ea
Zhao Li <zli385@uottawa.ca>
2026-04-01 12:24:21 -0400

    add config for simple shear test

--- src/main.cc
+++ src/main.cc
@@ -2775,7 +2775,7 @@ void PhaseFieldMonolithicSolve<LATraits, Tria>::make_grid_case_4()
     
     GridIn<dim> gridin;
     gridin.attach_triangulation(m_triangulation);
-    std::ifstream f("square_shear_structured.msh");
+    std::ifstream f(m_parameters.m_mesh_file_name);
     gridin.read_msh(f);
     
     //    for (const auto &cell : m_triangulation.active_cell_iterators())
